a0c00f559715b6261da4cc8f761ec4b63036b654
Jessica <Jessica@LAPTOP-K9BIE3M3>
2024-11-27 12:35:04 +0800

    homePage

---
+++ welcomePage/src/main/java/com/mycompany/welcomepage/WelcomePage.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.welcomepage;
+
+/**
+ *
+ * @author Jessica
+ */
+public class WelcomePage {
+    public static void main(String[]args){
+    new homePage();
+
+
+    }
+}
